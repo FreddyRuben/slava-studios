43dd3b7b594ff1f14bd9e7c9bf8a99ceee56d7be
Freddy Ruben <freddyruben@outlook.com>
2023-03-25 21:25:39 -0600

    Se agregó el bxslider

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+    $('.bxslider').bxSlider({
+        auto: true,
+        autoControls: true,
+        stopAutoOnClick: true,
+        pager: true,
+        slideWidth: 600
+      });
+});
